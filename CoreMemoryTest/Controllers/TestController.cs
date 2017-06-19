86eb1d4901885d059e96640525157584d9fde9ae
Andreas Nauta Pedersen <andreas.nauta.pedersen@natmus.dk>
2017-06-19 15:12:45 +0200

    Initial commit

---
+++ CoreMemoryTest/Controllers/TestController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CoreMemoryTest.Controllers
+{
+    [Route("api/[controller]")]
+    public class TestController : Controller
+    {
+        /// <summary>
+        /// Posts a file. Use like this: http://localhost:62774/api/test/UploadFile
+        /// </summary>
+        /// <param name="file">The file.</param>
+        /// <returns>A http request 200.</returns>
+        [HttpPost("UploadFile")]
+        public async Task<IActionResult> PostFileAsync(IFormFile file)
+        {
+            // Retrieve storage account from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse("");
+
+            // Create the blob client.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Retrieve reference to a previously created container.
+            CloudBlobContainer blobContainer = blobClient.GetContainerReference("");
+
+            // Retrieve reference to a blob named as the file.
+            CloudBlockBlob blockBlob = blobContainer.GetBlockBlobReference(file.FileName);
+
+            using (Stream s = file.OpenReadStream())
+            {
+                await blockBlob.UploadFromStreamAsync(s);
+            }
+
+            return Ok("Fíle uploaded");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# test-asp-core-memory
+# Introduction
 Test repository created to enable testing of .NET Core memory consumption while uploading a file.
+
+# Usage
+Use like this: http://localhost:62774/api/test/UploadFile
