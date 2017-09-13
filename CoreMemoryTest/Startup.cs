ab909687c034e10cedbff707479af9c997cd3893
Andreas Nauta Pedersen <anp@natmus.dk>
2017-09-13 14:16:28 +0200

    Testing github
    This is a test to github
    
    #1

--- CoreMemoryTest/Startup.cs
+++ CoreMemoryTest/Startup.cs
@@ -25,7 +25,7 @@ namespace CoreMemoryTest
 
         public IConfigurationRoot Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container. Testing github.
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
